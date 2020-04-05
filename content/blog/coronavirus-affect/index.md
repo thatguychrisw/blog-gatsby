091c62ccfac0b2610458c1a8e6c260ffab1be99d
thatguychrisw <thatguychrisw@gmail.com>
2020-04-05 17:20:47 -0400

    Update coronavirus article

--- content/blog/coronavirus-affect/index.md
+++ content/blog/coronavirus-affect/index.md
@@ -4,7 +4,16 @@ date: "2020-04-05T22:12:03.284Z"
 description: "The coronoavirus' affect on software development remote culture"
 ---
 
+
 ### Observations
 Due to the Coronavirus, most (hopefully all!) software companies are now operating with entirely remote staff.  Decision making is now handled mostly in calls, leading to packed calendars and what’s being known as Zoom Fatigue.  Connecting with your peers needs to be planned, and decisions can be difficult to track down, comprehend, and act on.  
 
-These are a new set of challenges for most groups, and I believe we can address the oncoming burnout through effective asynchronous communication and organization.
+These are a new set of challenges for most groups, and I believe we can address the oncoming burnout through effective asynchronous communication and organization.  Below are a few strategies I’ve been experimenting with, I’ll update them as I continue to make new discoveries.
+
+### Making The Most of Calls
+To come
+
+### Communication Focused Culture
+To come
+
+![Zoom Fatigue](./conference_call.png)
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,83 +1,84 @@
 module.exports = {
-  siteMetadata: {
-    title: `Discoveries & Musings`,
-    author: {
-      name: `Christopher Willard`,
-      summary: `, a software engineer/manager, who lives in FL with his wife and two little girls.`,
+    siteMetadata: {
+        title: `Discoveries & Musings`,
+        author: {
+            name: `Christopher Willard`,
+            summary: `, a software engineer/manager, who lives in FL with his wife and two little girls.`,
+        },
+        description: `bloggin' about software engineering and family life`,
+        siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+        social: {
+            twitter: `chriswillardFL`,
+        },
     },
-    description: `bloggin' about software engineering and family life`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
-    social: {
-      twitter: `chriswillardFL`,
-    },
-  },
-  plugins: [
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/blog`,
-        name: `blog`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/assets`,
-        name: `assets`,
-      },
-    },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          {
-            resolve: `gatsby-remark-images`,
+    plugins: [
+        {
+            resolve: `gatsby-source-filesystem`,
             options: {
-              maxWidth: 590,
+                path: `${__dirname}/content/blog`,
+                name: `blog`,
             },
-          },
-          {
-            resolve: `gatsby-remark-responsive-iframe`,
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
             options: {
-              wrapperStyle: `margin-bottom: 1.0725rem`,
+                path: `${__dirname}/content/assets`,
+                name: `assets`,
             },
-          },
-          `gatsby-remark-prismjs`,
-          `gatsby-remark-copy-linked-files`,
-          `gatsby-remark-smartypants`,
-        ],
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
-      },
-    },
-    `gatsby-plugin-feed`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
-        start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
-      },
-    },
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography`,
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
+        },
+        {
+            resolve: `gatsby-transformer-remark`,
+            options: {
+                plugins: [
+                    {
+                        resolve: `gatsby-remark-images`,
+                        options: {
+                            maxWidth: 590,
+                            quality: 100,
+                        },
+                    },
+                    {
+                        resolve: `gatsby-remark-responsive-iframe`,
+                        options: {
+                            wrapperStyle: `margin-bottom: 1.0725rem`,
+                        },
+                    },
+                    `gatsby-remark-prismjs`,
+                    `gatsby-remark-copy-linked-files`,
+                    `gatsby-remark-smartypants`,
+                ],
+            },
+        },
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+                //trackingId: `ADD YOUR TRACKING ID HERE`,
+            },
+        },
+        `gatsby-plugin-feed`,
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `Gatsby Starter Blog`,
+                short_name: `GatsbyJS`,
+                start_url: `/`,
+                background_color: `#ffffff`,
+                theme_color: `#663399`,
+                display: `minimal-ui`,
+                icon: `content/assets/gatsby-icon.png`,
+            },
+        },
+        `gatsby-plugin-react-helmet`,
+        {
+            resolve: `gatsby-plugin-typography`,
+            options: {
+                pathToConfigModule: `src/utils/typography`,
+            },
+        },
+        // this (optional) plugin enables Progressive Web App + Offline functionality
+        // To learn more, visit: https://gatsby.dev/offline
+        // `gatsby-plugin-offline`,
+    ],
 }
